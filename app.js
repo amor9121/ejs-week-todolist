4c9aad0890de81455612798a27f659977bc8a5f4
amor9121 <amor9121@gmail.com>
2022-09-14 17:28:36 +0800

    templating

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -12,63 +16,36 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    
-    var today = new Date();
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = date.getDate();
 
-    
-
-    // var currentDay = today.getDay();
-    // var day = "";
-
-    // switch(currentDay){
-    //     case 0:
-    //         day = "Sunday";
-    //         break;
-    //     case 1:
-    //         day = "Monday";
-    //         break;
-    //     case 2:
-    //         day = "Tuesday";
-    //         break;
-    //     case 3:
-    //         day = "Wednesday";
-    //         break;
-    //     case 4:
-    //         day = "Thursday";
-    //         break;
-    //     case 5:
-    //         day = "Friday";
-    //         break;
-    //     case 6:
-    //         day = "Saturday";
-    //         break;
-    //     default:
-    //         console.log("Error: current day is equal to : " + currentDay);
-    // }
-
-    // if(currentDay === 6 || currentDay === 0){
-    //     day = "weekend";
-    // } else {
-    //     day = "weekday";
-    // }
-
-    res.render("list", {kindOfDay: day, newListItems: items});
-
-
+    res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res){
-    var item = req.body.newItem;
-    items.push(item);
+    let item = req.body.newItem;
 
-    res.redirect("/");
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
+})
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+})
+
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
+
+app.get("/about", function(req, res){
+    res.render("about");
 })
 
 app.listen(3000, function(){
